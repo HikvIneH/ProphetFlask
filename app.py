9f7b092ff4b13e737cd960ef10b4401704a958fd
Hikvineh <rarifky.aditya@gmail.com>
2018-10-30 00:49:22 +0700

    edit config and app

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config.from_pyfile('config.py')
+#app.config.from_pyfile('config.py')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 
 db = SQLAlchemy(app)
 
 from views import *
 
 if __name__ == "__main__":
-	app.run(host='localhost',port=3000)
+	app.run_server(debug=True)
+	#app.run(host='localhost',port=3000)
