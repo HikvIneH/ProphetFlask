6c3b74843db5ae98120f89ee9060bd3f2ec23c94
Hikvineh <rarifky.aditya@gmail.com>
2018-10-30 01:32:49 +0700

    change app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask-heroku import Heroku
+from flask_heroku import Heroku
 
 app = Flask(__name__)
 heroku = Heroku(app)
@@ -13,5 +13,6 @@ db = SQLAlchemy(app)
 from views import *
 
 if __name__ == "__main__":
-	app.run_server(debug=True)
+	heroku.init_app(app)
+	#app.run_server(debug=True)
 	#app.run(host='localhost',port=3000)
