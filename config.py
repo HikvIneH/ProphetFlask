6cf4aaabadd6970212b0f15eb7554bfae257c21c
Hikvineh <rarifky.aditya@gmail.com>
2018-10-30 01:18:18 +0700

    change database url

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'mysql://root:taramitha@127.0.0.1/predict_app'
+#SQLALCHEMY_DATABASE_URI = 'mysql://root:taramitha@127.0.0.1/predict_app'
+SQLALCHEMY_DATABASE_URI = 'postgres://bwzqslyzlvdwvf:a787c5e75ba66ca96a3a0ed105c0f18ca8893cf94151c714131db71b1f3305a0@ec2-54-83-38-174.compute-1.amazonaws.com:5432/d30n18efmll675'
+
 SECRET_KEY = "Thisisasecret"
