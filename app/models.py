aa75940b622432b60ea1ef4611a800e1c316e27e
Hikvineh <rarifky.aditya@gmail.com>
2018-11-01 11:19:57 +0700

    fix config

--- app/models.py
+++ app/models.py
@@ -3,61 +3,25 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 from app import db, login_manager
 
-'''
-class User(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	email = db.Column(db.String(120), unique=True)
-	username = db.Column(db.String(50), unique=True)
-	password = db.Column(db.String(80))
-
-	def __init__(self, username, email, password):
-		self.username = username
-		self.email = email
-		self.password = password
-'''
-
 class User(UserMixin, db.Model):
-    """
-    Create an Employee table
-    """
-
-    # Ensures table will be named in plural and not in singular
-    # as is the name of the model
     __tablename__ = 'users'
-    '''
-	id = db.Column(db.Integer, primary_key=True)
-	email = db.Column(db.String(120), unique=True)
-	username = db.Column(db.String(50), unique=True)
-	password = db.Column(db.String(80))
-    '''
+
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(60), index=True, unique=True)
     username = db.Column(db.String(60), index=True, unique=True)
     first_name = db.Column(db.String(60), index=True)
     last_name = db.Column(db.String(60), index=True)
     password_hash = db.Column(db.String(128))
-    #history_id = db.Column(db.Integer, db.ForeignKey('history.id'))
-    #role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-    #is_admin = db.Column(db.Boolean, default=False)
 
     @property
     def password(self):
-        """
-        Prevent pasword from being accessed
-        """
         raise AttributeError('password is not a readable attribute.')
 
     @password.setter
     def password(self, password):
-        """
-        Set password to a hashed password
-        """
         self.password_hash = generate_password_hash(password)
 
     def verify_password(self, password):
-        """
-        Check if hashed password matches actual password
-        """
         return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
